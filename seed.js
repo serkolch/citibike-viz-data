a97e5dfaeaad40a465f56d31c526af059183c739
Sergey Kolchinskiy <sergey.kolchinskiy@gmail.com>
2016-01-25 09:23:38 -0500

    merge

--- seed.js
+++ seed.js
@@ -1,10 +1,7 @@
-var fs = require('fs')
 var glob = require('glob')
 
 glob("data/*.json", function(er,files){
   var dataFiles = files
-  console.log(dataFiles)
-  // var dataFiles = ['./data/02:00PM.json','./data/02:15PM.json','./data/02:30PM.json','./data/02:45PM.json']
 
   var requiredData = dataFiles.map(function(file){
     return require('./'+file)
@@ -23,27 +20,27 @@ glob("data/*.json", function(er,files){
     db = database;
     db.collection('bikes').remove({})
 
-    var structure = [];
+    var structure = {};
     requiredData[0].forEach(function(station){
-      structure.push({
+      structure[station.id] = {
         latitude: station.latitude,
         longitude: station.longitude,
         stationName: station.stationName,
         capacity: station.totalDocks,
-        stationId: station.id,
-        saturation:[]
-      })
+        averageBikes:{}
+      }
     })
-
-    db.collection('bikes').insert(structure)
 
     var repeat = function (file,index){
       var time = dataFiles[index].split(/[\/.]+/)[1]
+      var time = time.replace(":","-")
       file.forEach(function(station){
-        db.collection('bikes').update({stationId: station.id},{$push: {saturation: {hour: time, bikes: station.availableBikes}}})
+        structure[station.id].averageBikes[time] = station.availableBikes
       })
     }
     requiredData.forEach(repeat)
+
+    db.collection('bikes').insert(structure)
 
     process.on('exit', db.close);
   });
